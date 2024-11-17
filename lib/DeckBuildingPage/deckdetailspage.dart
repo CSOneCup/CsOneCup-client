393b8ae4551d0cdeccaf77a9676a375df6c1fbcd
Jaehwan Hyun <jec0101@naver.com>
2024-11-17 16:32:34 +0900

    add: 덱 상세 페이지 추가

---
+++ lib/DeckBuildingPage/deckdetailspage.dart
@@ -0,0 +1,19 @@
+import "package:flutter/material.dart";
+
+class DeckDetailsPage extends StatefulWidget {
+  const DeckDetailsPage({super.key});
+
+  @override
+  State<DeckDetailsPage> createState() => _DeckDetailsPageState();
+}
+
+class _DeckDetailsPageState extends State<DeckDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("TO BE DEVELOPED"),
+      ),
+    );
+  }
+}
