433682d829cab3330b4e97844efc7981a453de4d
EpicFn <collegeruled3@naver.com>
2024-11-17 19:49:50 +0900

    [update] bottomNavigationBar 구현

---
+++ lib/dummyPages/dummy_page_a.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DummyPageA extends StatelessWidget {
+  const DummyPageA({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Dummy Page A'));
+  }
+}
---
+++ lib/dummyPages/dummy_page_d.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DummyPageD extends StatelessWidget {
+  const DummyPageD({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Dummy Page D'));
+  }
+}
