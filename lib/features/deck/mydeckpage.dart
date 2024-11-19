851d2ba4d2af83261ad75bf6c02833b450b198af
Hyeok Jin Kim <71928299+EpicFn@users.noreply.github.com>
2024-11-19 20:27:46 +0900

    [Hotfix#14] dev 브랜치 최신 커밋 실행 오류 수정 (#15)
    * [fix] 실행 오류 수정 (assets 경로 설정, ListView 오류 등)
    
    * [fix] singuppage, landingpage 이미지 경로 변경
    
    ---------
    
    Co-authored-by: kingsorder <rlagurwls056@gmail.com>

--- lib/features/deck/mydeckpage.dart
+++ lib/features/deck/mydeckpage.dart
@@ -4,26 +4,29 @@ import 'package:cs_onecup/core/constants/colors.dart';
 import '../../data/models/deck.dart';
 
 class DeckCreatePage extends StatelessWidget {
+  const DeckCreatePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     throw UnimplementedError();
   }
-  
 }
 
 class MyDeckPage extends StatefulWidget {
+  const MyDeckPage({super.key});
+
   @override
   State<MyDeckPage> createState() => _MyDeckPageState();
 }
 
 class _MyDeckPageState extends State<MyDeckPage> {
   final _deckSearchController = TextEditingController();
-  List _originalDeckList = <Deck>[];   // 실제 데이터
+  final List _originalDeckList = <Deck>[]; // 실제 데이터
   List _deckList = <Deck>[];
-  String _searchText = '';
+  final String _searchText = '';
 
   int countListLength() {
-    if(_deckList.length % 3 == 0) {
+    if (_deckList.length % 3 == 0) {
       return _deckList.length ~/ 3;
     }
 
@@ -35,7 +38,7 @@ class _MyDeckPageState extends State<MyDeckPage> {
 
     // 덱 이름 기반 필터링
     _originalDeckList.where((deck) {
-      if(deck.getDeckName() == searchText ||
+      if (deck.getDeckName() == searchText ||
           deck.getDeckName().toString().contains(searchText)) {
         return true;
       } else {
@@ -45,9 +48,8 @@ class _MyDeckPageState extends State<MyDeckPage> {
 
     // 덱 태그 기반 필터링
     _originalDeckList.where((deck) {
-      if(deck.getTagList().toString().contains(searchText) &&
-          !deckList.contains(deck)
-      ) {
+      if (deck.getTagList().toString().contains(searchText) &&
+          !deckList.contains(deck)) {
         return true;
       } else {
         return false;
@@ -75,13 +77,13 @@ class _MyDeckPageState extends State<MyDeckPage> {
             controller: _deckSearchController,
             cursorColor: AppColors.mainDeepOrange,
             decoration: InputDecoration(
-              hintText: '검색어를 입력해주세요',
-              hintStyle: const TextStyle(fontSize: 12, color: AppColors.mainDeepOrange),
-              suffixIcon: Icon(Icons.search),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-              )
-            ),
+                hintText: '검색어를 입력해주세요',
+                hintStyle: const TextStyle(
+                    fontSize: 12, color: AppColors.mainDeepOrange),
+                suffixIcon: const Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                )),
             onChanged: (text) {
               setState(() {
                 _deckList = searchDeckList(text);
@@ -91,53 +93,67 @@ class _MyDeckPageState extends State<MyDeckPage> {
           const SizedBox(
             height: 20,
           ),
-          ListView.builder(
-            itemCount: countListLength(),
-            itemBuilder: (c, i) {
-              return Row(
-                children: [
-                  Column(
+          Expanded(
+            child: ListView.builder(
+                itemCount: countListLength(),
+                itemBuilder: (c, i) {
+                  return Row(
                     children: [
-                      Image.asset(
-                        'images/empty_deck.png',
-                        width: 50,
-                        height: 60,
+                      Column(
+                        children: [
+                          Image.asset(
+                            'images/empty_deck.png',
+                            width: 50,
+                            height: 60,
+                          ),
+                          Text(
+                            _deckList[i * 3].getDeckName(),
+                            style: const TextStyle(fontSize: 10),
+                          )
+                        ],
                       ),
-                      Text(_deckList[i * 3].getDeckName(), style: TextStyle(fontSize: 10),)
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Image.asset(
-                        'images/empty_deck.png',
-                        width: 50,
-                        height: 60,
+                      Column(
+                        children: [
+                          Image.asset(
+                            'images/empty_deck.png',
+                            width: 50,
+                            height: 60,
+                          ),
+                          Text(
+                            _deckList[i * 3 + 1].getDeckName(),
+                            style: const TextStyle(fontSize: 10),
+                          )
+                        ],
                       ),
-                      Text(_deckList[i * 3 + 1].getDeckName(), style: TextStyle(fontSize: 10),)
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Image.asset(
-                        'images/empty_deck.png',
-                        width: 50,
-                        height: 60,
+                      Column(
+                        children: [
+                          Image.asset(
+                            'images/empty_deck.png',
+                            width: 50,
+                            height: 60,
+                          ),
+                          Text(
+                            _deckList[i * 3 + 2].getDeckName(),
+                            style: const TextStyle(fontSize: 10),
+                          )
+                        ],
                       ),
-                      Text(_deckList[i * 3 + 2].getDeckName(), style: TextStyle(fontSize: 10),)
                     ],
-                  ),
-                ],
-              );
-            }
+                  );
+                }),
           ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) => DeckCreatePage()));
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const DeckCreatePage()));
         },
         tooltip: 'Create Deck',
-        child: const Icon(Icons.add, size: 20,),
+        child: const Icon(
+          Icons.add,
+          size: 20,
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,22 @@
 import 'package:cs_onecup/features/auth/landingpage.dart';
 import 'package:flutter/material.dart';
+import 'package:cs_onecup/features/main/mainpage.dart';
 
 void main() {
-  runApp(LandingPage());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CsOneCup',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LandingPage(),
+    );
+  }
 }
