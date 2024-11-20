76dbb8de7de4d537b13d01f453ae7d60e78b3b40
kingsorder <rlagurwls056@gmail.com>
2024-11-20 23:44:29 +0900

    add : 손패 widget 구현

--- lib/features/home/homepage.dart
+++ lib/features/home/homepage.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cs_onecup/core/constants/colors.dart';
 import 'package:cs_onecup/features/home/userprofile.dart';
 import 'package:cs_onecup/features/home/solved_and_retry_problem_info.dart';
-import 'package:cs_onecup/core/widgets/simplecardwidget.dart';
+import 'package:cs_onecup/features/home/cards_in_hand.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -20,15 +20,29 @@ class _HomePageState extends State<HomePage> {
         width: double.infinity,
         height: double.infinity,
         color: AppColors.mainLightGray,
-        child: Column(
-          children: [
-            UserProfile(userName: '사용자', userLevel: 5, userExp: exp),
-            SolvedAndRetryProblemsInfo(
-              solvedProblemsToday: 5,
-              retryProblems: 12,
-            ),
-            const SimpleCardwidget(),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              UserProfile(userName: '사용자', userLevel: 5, userExp: exp),
+              SolvedAndRetryProblemsInfo(
+                solvedProblemsToday: 5,
+                retryProblems: 12,
+              ),
+              Image.asset(
+                'assets/icons/icon_arrow_up.png',
+                width: 70,
+                height: 70,
+                color: AppColors.mainDeepOrange,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const CardsInHand(),
+              const SizedBox(
+                height: 70,
+              ),
+            ],
+          ),
         ));
   }
 }
