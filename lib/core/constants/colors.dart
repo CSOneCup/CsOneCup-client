cd9ae5dc37c21545c42a9e7604dd49e664453f74
Jaehwan Hyun <jec0101@naver.com>
2024-11-24 21:02:56 +0900

    update: 색 추가 (폰트용 갈색)

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -9,4 +9,5 @@ class AppColors {
   static const mainDarkGray = Color(0xFFD9D9D9);
   static const lightRed = Color(0xFFE46767);
   static const cardBeige = Color(0xFFEFD9B6);
+  static const textBrown = Color(0xFFE1752F);
 }
