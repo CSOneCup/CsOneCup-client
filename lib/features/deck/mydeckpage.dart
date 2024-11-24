fade887edf8f05d6b005ba64adc4c38bcafeefff
SHKim55 <5061ksh@naver.com>
2024-11-24 16:36:17 +0900

    feat : add page routing & enhance UI Layout

--- lib/features/deck/mydeckpage.dart
+++ lib/features/deck/mydeckpage.dart
@@ -1,16 +1,8 @@
-import 'package:flutter/cupertino.dart';
+import 'package:cs_onecup/features/deck/deckcreatepage.dart';
 import 'package:flutter/material.dart';
 import 'package:cs_onecup/core/constants/colors.dart';
 import '../../data/models/deck.dart';
-
-class DeckCreatePage extends StatelessWidget {
-  const DeckCreatePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    throw UnimplementedError();
-  }
-}
+import 'deckdetailpage.dart';
 
 class MyDeckPage extends StatefulWidget {
   const MyDeckPage({super.key});
@@ -21,8 +13,18 @@ class MyDeckPage extends StatefulWidget {
 
 class _MyDeckPageState extends State<MyDeckPage> {
   final _deckSearchController = TextEditingController();
-  final List _originalDeckList = <Deck>[]; // 실제 데이터
-  List _deckList = <Deck>[];
+  final List _originalDeckList = <Deck>[
+    Deck(0, '선호', '테스트 덱 1', 3),
+    Deck(1, '재훈', '테스트 덱 2', 3),
+    Deck(2, '재환', '테스트 덱 3', 3),
+    Deck(3, '혁진', '테스트 덱 4', 3),
+  ]; // 실제 데이터
+  List _deckList = <Deck>[
+    Deck(0, '선호', '테스트 덱 1', 3),
+    Deck(1, '재훈', '테스트 덱 2', 3),
+    Deck(2, '재환', '테스트 덱 3', 3),
+    Deck(3, '혁진', '테스트 덱 4', 3),
+  ];
   final String _searchText = '';
 
   int countListLength() {
@@ -34,7 +36,7 @@ class _MyDeckPageState extends State<MyDeckPage> {
   }
 
   List searchDeckList(String searchText) {
-    List deckList = <Deck>[];
+    List<Deck> deckList = <Deck>[];
 
     // 덱 이름 기반 필터링
     _originalDeckList.where((deck) {
@@ -67,93 +69,104 @@ class _MyDeckPageState extends State<MyDeckPage> {
 
   @override
   Widget build(BuildContext context) {
+    double buttonWidth = MediaQuery.of(context).size.width * 0.8;
+
     return Scaffold(
       body: Column(
         children: [
           const SizedBox(
-            height: 10,
+            height: 15,
           ),
-          TextField(
-            controller: _deckSearchController,
-            cursorColor: AppColors.mainDeepOrange,
-            decoration: InputDecoration(
-                hintText: '검색어를 입력해주세요',
-                hintStyle: const TextStyle(
-                    fontSize: 12, color: AppColors.mainDeepOrange),
-                suffixIcon: const Icon(Icons.search),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                )),
-            onChanged: (text) {
-              setState(() {
-                _deckList = searchDeckList(text);
-              });
-            },
+          Padding(
+            padding: EdgeInsets.only(left: buttonWidth / 16, right: buttonWidth / 16, top: 5, bottom: 5),
+            child: SizedBox(
+              height: 45,
+              child: TextField(
+                controller: _deckSearchController,
+                cursorColor: AppColors.mainDeepOrange,
+                textAlignVertical: TextAlignVertical.center,
+                decoration: InputDecoration(
+                  fillColor: AppColors.mainDeepOrange,
+                  prefixIconColor: AppColors.mainDeepOrange,
+                  suffixIconColor: AppColors.mainDeepOrange,
+                  focusColor: AppColors.mainDeepOrange,
+                  hintText: '검색어를 입력해주세요',
+                  hintStyle: const TextStyle(
+                    fontSize: 15,
+                    color: AppColors.mainDeepOrange),
+                  suffixIcon: const Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(
+                      color: AppColors.mainDeepOrange,
+                      width: 1.5
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(
+                        color: AppColors.mainDeepOrange,
+                        width: 2.0
+                    ),
+                  ),
+                ),
+                onChanged: (text) {
+                  setState(() {
+                    _deckList = searchDeckList(text);
+                  });
+                },
+              ),
+            ),
           ),
           const SizedBox(
             height: 20,
           ),
           Expanded(
-            child: ListView.builder(
-                itemCount: countListLength(),
-                itemBuilder: (c, i) {
-                  return Row(
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                childAspectRatio: 1 / 1.4,
+                mainAxisSpacing: 5,
+                crossAxisSpacing: 0.5
+              ),
+              itemCount: _deckList.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  child: Column(
                     children: [
-                      Column(
-                        children: [
-                          Image.asset(
-                            'images/empty_deck.png',
-                            width: 50,
-                            height: 60,
-                          ),
-                          Text(
-                            _deckList[i * 3].getDeckName(),
-                            style: const TextStyle(fontSize: 10),
-                          )
-                        ],
+                      GestureDetector(
+                        child: const Image(
+                          image: AssetImage('assets/images/emptyDeck.png')
+                        ),
+                        onTap: () {
+                          Navigator.push(context, MaterialPageRoute(
+                            builder: (context) => DeckDetailPage(deckId: _deckList[index].deckId),
+                          ));
+                        },
                       ),
-                      Column(
-                        children: [
-                          Image.asset(
-                            'images/empty_deck.png',
-                            width: 50,
-                            height: 60,
-                          ),
-                          Text(
-                            _deckList[i * 3 + 1].getDeckName(),
-                            style: const TextStyle(fontSize: 10),
-                          )
-                        ],
+                      const SizedBox(
+                        height: 5,
                       ),
-                      Column(
-                        children: [
-                          Image.asset(
-                            'images/empty_deck.png',
-                            width: 50,
-                            height: 60,
-                          ),
-                          Text(
-                            _deckList[i * 3 + 2].getDeckName(),
-                            style: const TextStyle(fontSize: 10),
-                          )
-                        ],
-                      ),
+                      Text(_deckList[index].name)
                     ],
-                  );
-                }),
+                  ),
+                );
+              }
+            )
           ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(context,
-              MaterialPageRoute(builder: (context) => const DeckCreatePage()));
+            MaterialPageRoute(builder: (context) => const DeckCreatePage()));
         },
         tooltip: 'Create Deck',
-        child: const Icon(
-          Icons.add,
-          size: 20,
-        ),
+        backgroundColor: AppColors.mainDeepOrange,
+        foregroundColor: Colors.white,
+        elevation: 25,
+        shape: const CircleBorder(),
+        child: const Icon(Icons.add, size: 30,),
       ),
     );
   }
