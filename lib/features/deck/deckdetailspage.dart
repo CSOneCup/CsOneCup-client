55753b19f66854ddb131472ebcd3ef59794688e8
Jaehwan Hyun <jec0101@naver.com>
2024-11-24 20:25:00 +0900

    update: deckDetailsPage 초벌 구이

--- lib/features/deck/deckdetailspage.dart
+++ lib/features/deck/deckdetailspage.dart
@@ -15,10 +15,68 @@ class _DeckDetailsPageState extends State<DeckDetailsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.mainBeige,
+      appBar: AppBar(
+        backgroundColor: AppColors.mainBeige,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.brown),
+          onPressed: () {
+            // 뒤로가기 버튼 동작
+            Navigator.pop(context);
+          }
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              // 편집 버튼 동작
+            },
+            child: const Text(
+              "편집",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.brown
+              ),
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              // 공유 버튼 동작
+            },
+            child: const Text(
+              "공유",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.brown
+              ),
+            ),
+          ),
+        ],
+      ),
+
       body: Column(
         children: [
-          // 덱 제목 여기에
-
+          // 덱 제목 & 카드 수
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: const [
+                Text(
+                  "나의 덱 이름",
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.brown,
+                  ),
+                ),
+                SizedBox(height: 4),
+                Text(
+                  "18 cards",
+                  style: TextStyle(fontSize: 16, color: Colors.grey),
+                ),
+              ],
+            ),
+          ),
+          // 카드 리스트
           Expanded(
               child: ListView.builder(
                 itemCount: 15,
@@ -39,45 +97,38 @@ class SimpleCardTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
-      child: Stack(
-        children: [
-          Container(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(color: Colors.black, width: 1),
+        ),
+        child: ListTile(
+          // 아이콘
+          leading: Container(
+            padding: const EdgeInsets.all(8),
             decoration: BoxDecoration(
-              color: Colors.blue, // 배경색
-              borderRadius: BorderRadius.circular(12), // 둥근 테두리
-              border: Border.all(color: Colors.black, width: 3), // 테두리
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8),
             ),
-            padding: EdgeInsets.all(5),
-            child: ListTile(
-                leading: Container(
-                  padding: EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: AppColors.mainBeige,
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Icon(Icons.format_list_bulleted, color: Colors.brown),
-                ),
-                title: const Center(
-                  child: Text(
-                    "카드 이름",
-                    style: TextStyle(fontSize: 18, color: Colors.red),
-                    textAlign: TextAlign.center, // 텍스트 가운데 정렬
-                  ),
-                ),
-                tileColor: AppColors.cardBeige,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                onTap: () {
-                  // 카드 클릭
-                  print("Card $index clicked");
-                }
+            child: const Icon(Icons.format_list_bulleted, color: Colors.brown),
+          ),
+          // 제목
+          title: Center(
+            child: const Text(
+              "간략한 카드 이름",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.brown,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
-
-        ],
-      )
-
+          onTap: () {
+            // 카드 클릭 이벤트
+          },
+        ),
+      ),
 
     );
   }
