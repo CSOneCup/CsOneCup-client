7b7536cde6c365bb38385fc94e26a10eab4d5710
SHKim55 <5061ksh@naver.com>
2024-11-24 21:50:39 +0900

    refactor : remove unused package import

--- lib/features/challenge/challengepage.dart
+++ lib/features/challenge/challengepage.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:cs_onecup/data/models/deck.dart';
 import 'package:flutter/material.dart';
 import 'package:cs_onecup/core/constants/colors.dart';
