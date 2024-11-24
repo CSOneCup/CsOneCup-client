0a5f711e4d502e50a527f3265bcf64e60c72cd60
SHKim55 <5061ksh@naver.com>
2024-11-24 16:33:40 +0900

    refactor : change primarySwatch color

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'CsOneCup',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
       ),
       home: const MainPage(),
     );
