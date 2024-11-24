30b0055179658c2186a1f507c3178a92814514db
Seonho Kim <68678863+SHKim55@users.noreply.github.com>
2024-11-24 22:41:37 +0900

    [Feat#8] 덱 구성, 챌린지 페이지 구현 (#22)
    * chore : add provider dependency
    
    * feat : add appbar & tab bar with box type indicator
    
    * refactor : rename image file
    
    * refactor : change primarySwatch color
    
    * refactor : change class name of 2nd tab in bottom navigation
    
    * feat : add demo pages for deck information
    
    * feat : add page routing & enhance UI Layout
    
    * config
     config: add provider configuration
    
    * config : add image assets
    
    * feat : add image assets
    
    * refactor : change background color
    
    * feat : add challenge page layout
    
    * refactor : remove unused package import

---
+++ lib/features/deck/deckcreatepage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:cs_onecup/core/constants/colors.dart';
+import '../../data/models/deck.dart';
+
+class DeckCreatePage extends StatelessWidget {
+  const DeckCreatePage({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Deck Create Page'),
+      ),
+    );
+  }
+}
