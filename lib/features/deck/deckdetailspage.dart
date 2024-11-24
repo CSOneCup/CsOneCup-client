63b087d891ed018bf3b218d1b2f542c32425116d
Jaehwan Hyun <jec0101@naver.com>
2024-11-24 20:16:39 +0900

    add: deckDetailsPage 추가

--- lib/features/deck/deckdetailspage.dart
+++ lib/features/deck/deckdetailspage.dart
@@ -1,3 +1,5 @@
+import "package:cs_onecup/core/constants/colors.dart";
+import "package:cs_onecup/core/widgets/simplecardwidget.dart";
 import "package:flutter/material.dart";
 
 class DeckDetailsPage extends StatefulWidget {
@@ -13,9 +15,70 @@ class _DeckDetailsPageState extends State<DeckDetailsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("TO BE DEVELOPED"),
-      ),
+      body: Column(
+        children: [
+          // 덱 제목 여기에
+
+          Expanded(
+              child: ListView.builder(
+                itemCount: 15,
+                itemBuilder: (context, index) => SimpleCardTile(index: index)
+              )
+          )
+        ],
+      )
+    );
+  }
+}
+
+class SimpleCardTile extends StatelessWidget {
+  final int index;
+  const SimpleCardTile({super.key, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
+      child: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.blue, // 배경색
+              borderRadius: BorderRadius.circular(12), // 둥근 테두리
+              border: Border.all(color: Colors.black, width: 3), // 테두리
+            ),
+            padding: EdgeInsets.all(5),
+            child: ListTile(
+                leading: Container(
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    color: AppColors.mainBeige,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Icon(Icons.format_list_bulleted, color: Colors.brown),
+                ),
+                title: const Center(
+                  child: Text(
+                    "카드 이름",
+                    style: TextStyle(fontSize: 18, color: Colors.red),
+                    textAlign: TextAlign.center, // 텍스트 가운데 정렬
+                  ),
+                ),
+                tileColor: AppColors.cardBeige,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                onTap: () {
+                  // 카드 클릭
+                  print("Card $index clicked");
+                }
+            ),
+          ),
+
+        ],
+      )
+
+
     );
   }
 }
