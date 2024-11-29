32ce6a0dbcafaa34665b4272e7fa3edcac0df086
Jaehwan Hyun <jec0101@naver.com>
2024-11-29 17:51:09 +0900

    fix: dev 브랜치 merge에 따른 변경점

--- lib/features/deck/deckdetailspage.dart
+++ lib/features/deck/deckdetailspage.dart
@@ -1,10 +1,10 @@
 import "package:cs_onecup/core/constants/colors.dart";
-import "package:cs_onecup/core/widgets/simplecardwidget.dart";
+import "package:cs_onecup/features/deck/widgets/simplecardtile.dart";
 import "package:flutter/material.dart";
+
 
 class DeckDetailsPage extends StatefulWidget {
   const DeckDetailsPage({super.key});
-
   @override
   State<DeckDetailsPage> createState() => _DeckDetailsPageState();
 }
@@ -93,92 +93,3 @@ class _DeckDetailsPageState extends State<DeckDetailsPage> {
   }
 }
 
-/// 카드 타일 위젯
-class SimpleCardTile extends StatelessWidget {
-  final int index;
-  const SimpleCardTile({super.key, required this.index});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
-      child: Container(
-        // 바깥 컨테이너 (흰색)
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(20),
-          border: Border.all(color: Colors.black, width: 2.5),
-        ),
-        child: Padding(
-          // 내부 컨테이너 (베이지색)
-          padding: const EdgeInsets.all(4.0), // 흰 테두리 두꼐
-          child: Container(
-            decoration: BoxDecoration(
-              color: AppColors.cardBeige,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: ListTile(
-              // 아이콘
-              leading: Container(
-                padding: const EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: const Icon(Icons.format_list_bulleted, color: Colors.brown),
-              ),
-              // 제목
-              title: Center(
-                child: const Text(
-                  "간략한 카드 이름",
-                  style: TextStyle(
-                    fontSize: 25,
-                    color: AppColors.mainDeepOrange,
-                    fontWeight: FontWeight.normal,
-                  ),
-                ),
-              ),
-              onTap: () {
-                // 카드 클릭 이벤트
-              },
-            ),
-          ),
-        ),
-      ),
-
-    );
-  }
-}
-
-/**
- *  Expanded(
-    child: ListView.builder(
-    itemCount: 10, // 카드 개수
-    itemBuilder: (context, index) {
-    return Padding(
-    padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
-    child: ListTile(
-    leading: Container(
-    padding: EdgeInsets.all(8),
-    decoration: BoxDecoration(
-    color: Colors.grey[300],
-    borderRadius: BorderRadius.circular(8),
-    ),
-    child: Icon(Icons.format_list_bulleted, color: Colors.brown),
-    ),
-    title: Text(
-    "간략한 카드 이름",
-    style: TextStyle(fontSize: 18, color: Colors.brown),
-    ),
-    tileColor: Colors.amber[100],
-    shape: RoundedRectangleBorder(
-    borderRadius: BorderRadius.circular(12),
-    ),
-    onTap: () {
-    // 카드 클릭 동작
-    print('Card $index clicked');
-    },
-    ),
-    );
-    },
-    ),
- * */
