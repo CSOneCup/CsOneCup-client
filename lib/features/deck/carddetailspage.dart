9758dfa9beb6e9ef5d447471b19c56390e1eaf31
Jaehwan Hyun <jec0101@naver.com>
2024-12-01 17:45:35 +0900

    fix: CardDetailsPage 업데이트 - cardTitle 프로퍼티 추가 - 예전에 적었던 필요없는 코드 삭제

--- lib/features/deck/carddetailspage.dart
+++ lib/features/deck/carddetailspage.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 
 
 class CardDetailsPage extends StatefulWidget {
+  final String _cardTitle;
   final String _quizCategory;
   final String _quizExplanation;
   final String _quizAnswer;
@@ -14,11 +15,13 @@ class CardDetailsPage extends StatefulWidget {
 
   const CardDetailsPage({
     super.key,
+    required String cardTitle,
     required String quizCategory,
     required String quizExplanation,
     required String quizAnswer,
     required String answerExplanation})
-  :_quizCategory = quizCategory,
+  :_cardTitle = cardTitle,
+   _quizCategory = quizCategory,
    _quizExplanation = quizExplanation,
    _quizAnswer = quizAnswer,
    _answerExplanation = answerExplanation;
@@ -33,98 +36,80 @@ class _CardDetailsPageState extends State<CardDetailsPage> with SingleTickerProv
 
 
   // 카드 위젯 관련
-  bool _isFront = true;
-  late AnimationController _controller;
   final GlobalKey<FlipCardState> _cardKey = GlobalKey<FlipCardState>();
   final _cardScaleFactor = 2.0;
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
-      duration: const Duration(seconds: 1),
-      vsync: this,
-    );
   }
 
   @override
   void dispose() {
-    _controller.dispose();
     super.dispose();
   }
 
-  /** 카드 회전 */
-  void flipCard() {
-    if (_isFront) {
-      _controller.forward();
-    } else {
-      _controller.reverse();
-    }
-    setState(() {
-      _isFront = !_isFront;
-    });
-  }
-
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.mainBeige,
-      appBar: AppBar(
-        elevation: 0,
+    return GestureDetector(
+      onTap: () {
+        print("Gesture Detector Tap");
+        Navigator.pop(context);
+      },
+      child: Scaffold(
         backgroundColor: AppColors.mainBeige,
-        leading: IconButton(onPressed: (){}, icon: const Icon(Icons.arrow_back_ios_new, color: Colors.brown,)),
-      ),
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: AppColors.mainBeige,
+          leading: IconButton(onPressed: (){ Navigator.pop(context); }, icon: const Icon(Icons.arrow_back_ios_new, color: Colors.brown,)),
+        ),
 
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
 
-        children: [
-          // 카드 제목
-          Center(
-            child: Text("카드 제목",
-              style: TextStyle(
-                fontSize: 35,
-                color: AppColors.mainDeepOrange
+          children: [
+            // 카드 제목
+            Center(
+              child: Text(widget._cardTitle,
+                style: const TextStyle(
+                  fontSize: 35,
+                  color: AppColors.mainDeepOrange
+                ),
               ),
             ),
-          ),
 
-          SizedBox(
-            height: 8,
-          ),
+            const SizedBox(height: 8),
 
-          // 카드
-          FlipCard(
-            key: _cardKey,
-            direction: FlipDirection.HORIZONTAL,
-            side: CardSide.FRONT,
-            front: QuizCardwidget(
-              scaleFactor: _cardScaleFactor,
-              quizCategory: widget._quizCategory,
-              quizExplanation: widget._quizExplanation
+            // 카드
+            FlipCard(
+              key: _cardKey,
+              direction: FlipDirection.HORIZONTAL,
+              side: CardSide.FRONT,
+              front: QuizCardwidget(
+                scaleFactor: _cardScaleFactor,
+                quizCategory: widget._quizCategory,
+                quizExplanation: widget._quizExplanation
+              ),
+              back: AnswerCardwidget(
+                scaleFactor: _cardScaleFactor,
+                quizAnswer: widget._quizAnswer,
+                answerExplanation: widget._answerExplanation
+              )
             ),
-            back: AnswerCardwidget(
-              scaleFactor: _cardScaleFactor,
-              quizAnswer: widget._quizAnswer,
-              answerExplanation: widget._answerExplanation
+
+            const SizedBox(height: 16),
+
+            // 레이블 (눌러서 답 보기)
+            Text("눌러서 답 보기",
+              style: TextStyle(
+                fontSize: 24,
+                color: AppColors.mainDeepOrange.withOpacity(0.3),
+              ),
             )
-          ),
-
-          SizedBox(
-            height: 8,
-          ),
-
-          // 레이블 (눌러서 답 보기)
-          Text("눌러서 답 보기",
-            style: TextStyle(
-              color: AppColors.mainDeepOrange,
-            ),
-            // TODO 투명도
-          )
 
 
-        ],
+          ],
+        ),
       ),
     );
   }
