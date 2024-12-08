b3d384d66bda94cf275944043991fb60baadf4f1
SHKim55 <5061ksh@naver.com>
2024-12-09 02:05:57 +0900

    refactor : add server connection & adapt FutureBuilder

--- lib/features/ranking/rankingpage.dart
+++ lib/features/ranking/rankingpage.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:cs_onecup/core/constants/colors.dart';
 import 'package:cs_onecup/features/home/userprofile.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 
 class RankingPage extends StatefulWidget {
   const RankingPage({super.key});
@@ -10,22 +14,106 @@ class RankingPage extends StatefulWidget {
 }
 
 class _RankingPageState extends State<RankingPage> {
-  DateTime _dateTime = DateTime.now();
-  UserProfile _userProfile = UserProfile(userLevel: 23, userName: 'TestUser', userExp: 2100);
-  int rank = 8;
-  List<UserProfile> rankedUserList = <UserProfile>[
-    UserProfile(userLevel: 30, userName: 'Test1', userExp: 5000),
-    UserProfile(userLevel: 29, userName: 'Test2', userExp: 4500),
-    UserProfile(userLevel: 28, userName: 'Test3', userExp: 4000),
-    UserProfile(userLevel: 27, userName: 'Test4', userExp: 3500),
-    UserProfile(userLevel: 26, userName: 'Test5', userExp: 3000),
-    UserProfile(userLevel: 25, userName: 'Test6', userExp: 2500),
-    UserProfile(userLevel: 24, userName: 'Test7', userExp: 2300),
-    UserProfile(userLevel: 24, userName: 'TestUser', userExp: 2100),
-  ];
+  final String url = "http://141.164.52.130:8082";
+  final DateTime _dateTime = DateTime.now();
+  // UserProfile _userProfile = UserProfile(userLevel: 23, userName: 'TestUser', userExp: 2100);
+  Future<UserProfile?>? _userProfile;
+  Future<List<UserProfile?>?>? _rankedUserList;
+  int rank = 1;
 
-  void _calculateRank() {
-    // 여기에 순위 직접 계산 or 서버로부터 받아오는 로직 작성
+  @override
+  void initState() {
+    _initializeRankingData();
+    _userProfile = _loadUserData();
+    _rankedUserList = _loadAllUserData();
+    super.initState();
+  }
+
+  Future<void> _initializeRankingData() async {
+    try {
+      final user = await _loadUserData();
+      final rankedList = await _loadAllUserData();
+
+      if (rankedList != null && user != null) {
+        for (int i = 0; i < rankedList.length; i++) {
+          if (rankedList[i]?.userName == user.userName) {
+            setState(() {
+              rank = i + 1;
+            });
+            break;
+          }
+        }
+      }
+    } catch (e) {
+      print('Ranking data 초기화 중 오류 발생: $e');
+    }
+  }
+
+  Future<UserProfile?> _loadUserData() async {
+    final String requestUrl = "$url/api/user/info";
+    final sharedPreference = await SharedPreferences.getInstance();
+    final jwtToken = sharedPreference.getString('authToken');
+
+    try {
+      final response = await http.get(
+        Uri.parse(requestUrl),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $jwtToken'
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> responseData = jsonDecode(response.body);
+        final String userName = responseData['data']['name'];
+        final int level = responseData['data']['level'];
+        final int expPoint = responseData['data']['exp_point'];
+
+        return UserProfile(userName: userName, userLevel: level, userExp: expPoint,);
+      }
+    } catch(e) {
+      print('오류 발생 - UserProfile');
+      print(e);
+      return null;
+    }
+    return null;
+  }
+
+  Future<List<UserProfile?>?> _loadAllUserData() async {
+    String requestUrl = "$url/api/user/all/info";
+    final sharedPreference = await SharedPreferences.getInstance();
+    final jwtToken = sharedPreference.getString('authToken');
+    List<UserProfile> userProfileList = [];
+
+    try {
+      final response = await http.get(
+        Uri.parse(requestUrl),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $jwtToken'
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> responseData = jsonDecode(response.body);
+        List<dynamic> userList = responseData['data'];  // map
+
+        userList.sort((mapA, mapB) {
+          final expA = mapA['exp_point'];
+          final expB = mapB['exp_point'];
+          return expB.compareTo(expA);
+        });
+
+        userList.forEach((user) {
+          userProfileList.add(UserProfile(userLevel: user['level'], userName: user['name'], userExp: user['exp_point']));
+        });
+        return userProfileList;
+      }
+    } catch(e) {
+      print(e);
+      return null;
+    }
+    return null;
   }
 
   Color _setRankingTextColor(int index) {
@@ -38,20 +126,6 @@ class _RankingPageState extends State<RankingPage> {
     } else {
       return Colors.black;
     }
-  }
-  
-  Color _setMyRankingFieldBackground(int index) {
-    if(rankedUserList[index].userName == _userProfile.userName) {
-      return AppColors.lightBeige;
-    } else {
-      return Colors.white;
-    }
-  }
-
-  @override
-  void initState() {
-    _calculateRank();
-    super.initState();
   }
 
   @override
@@ -75,7 +149,7 @@ class _RankingPageState extends State<RankingPage> {
                   image: AssetImage('assets/images/ranking_crown.png')
               ),
             ),
-            Text('${_dateTime.year}년 ${_dateTime.month}월', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+            Text('${_dateTime.year}년 ${_dateTime.month}월', style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
             Container(
               height: 20,
               child: const Image(
@@ -102,18 +176,39 @@ class _RankingPageState extends State<RankingPage> {
                       border: Border.all(color: AppColors.mainDeepOrange, width: 3),
                       borderRadius: BorderRadius.circular(8),
                     ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        const Text('레벨 순위              ',
-                          style: TextStyle(fontSize: 16, color: Colors.black,),),
-                        Text('전체 ${rank}위',
-                          style: const TextStyle(fontSize: 16, color: Colors.orange)),
-                        const Text('       |       ', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
-                        Text('Lv. ${_userProfile.userLevel}',
-                          style: const TextStyle(fontSize: 16, color: Colors.black),
-                        ),
-                      ],
+                    child: FutureBuilder(
+                      future: _userProfile,
+                      builder: (context, snapshot) {
+                        if(snapshot.hasData) {
+                          return Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              const Text('레벨 순위              ',
+                                style: TextStyle(fontSize: 16, color: Colors.black,),),
+                              Text('전체 ${rank}위',
+                                  style: const TextStyle(fontSize: 16, color: Colors.orange)),
+                              const Text('       |       ', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                              Text('Lv. ${snapshot.data?.userLevel}',
+                                style: const TextStyle(fontSize: 16, color: Colors.black),
+                              ),
+                            ],
+                          );
+                        } else {
+                          return const Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text('레벨 순위              ',
+                                style: TextStyle(fontSize: 16, color: Colors.black,),),
+                              Text('순위 없음',
+                                  style: TextStyle(fontSize: 16, color: Colors.orange)),
+                              Text('       |       ', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                              Text('Lv. ?',
+                                style: TextStyle(fontSize: 16, color: Colors.black),
+                              ),
+                            ],
+                          );
+                        }
+                      },
                     ),
                   ),
                   const SizedBox(
@@ -131,79 +226,88 @@ class _RankingPageState extends State<RankingPage> {
                       border: Border.all(color: AppColors.mainDeepOrange, width: 3),
                       borderRadius: BorderRadius.circular(8),
                     ),
-                    child: ListView.builder(
-                      itemCount: rankedUserList.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        if(index < 3) {
-                          return Container(
-                            color: _setMyRankingFieldBackground(index),
-                            child: Column(
-                              children: [
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    Container(
-                                      padding: EdgeInsets.only(left: 10),
-                                      height: 25,
-                                      child: const Image(
-                                          image: AssetImage('assets/icons/icon_gold_medal.png')
-                                      ),
+                    child: FutureBuilder(
+                      future: _rankedUserList,
+                      builder: (context, snapshot) {
+                        print(snapshot.toString());
+
+                        if(snapshot.hasData) {
+                          return ListView.builder(
+                              itemCount: snapshot.data?.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                if(index < 3) {
+                                  return Container(
+                                    child: Column(
+                                      children: [
+                                        Row(
+                                          mainAxisAlignment: MainAxisAlignment.start,
+                                          children: [
+                                            Container(
+                                              padding: EdgeInsets.only(left: 10),
+                                              height: 25,
+                                              child: const Image(
+                                                  image: AssetImage('assets/icons/icon_gold_medal.png')
+                                              ),
+                                            ),
+                                            Container(
+                                              width: 150,
+                                              height: 40,
+                                              alignment: Alignment.centerLeft,
+                                              child: Text('  ${snapshot.data?[index]?.userName}',
+                                                style: TextStyle(fontSize: 16, color: Colors.black,),),
+                                            ),
+                                            Text('${index + 1}위',
+                                                style: TextStyle(fontSize: 16, color: _setRankingTextColor(index))),
+                                            const Text('     |     ', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                                            Text('${snapshot.data?[index]?.userExp}  pts',
+                                              style: const TextStyle(fontSize: 16, color: Colors.black),
+                                            ),
+                                          ],
+                                        ),
+                                        Image(
+                                            width: MediaQuery.of(context).size.width * 0.75,
+                                            image: AssetImage('assets/images/line_divider.png')
+                                        ),
+                                      ],
                                     ),
-                                    Container(
-                                      width: 150,
-                                      height: 40,
-                                      alignment: Alignment.centerLeft,
-                                      child: Text('  ${rankedUserList[index].userName}',
-                                        style: TextStyle(fontSize: 16, color: Colors.black,),),
+                                  );
+                                } else {
+                                  return Container(
+                                    child: Column(
+                                      children: [
+                                        Row(
+                                          mainAxisAlignment: MainAxisAlignment.start,
+                                          children: [
+                                            Container(
+                                              width: 150,
+                                              height: 40,
+                                              margin: EdgeInsets.only(left: 35),
+                                              alignment: Alignment.centerLeft,
+                                              child: Text('  ${snapshot.data?[index]?.userName}',
+                                                style: TextStyle(fontSize: 16, color: Colors.black,),),
+                                            ),
+                                            Text('${index + 1}위',
+                                                style: TextStyle(fontSize: 16, color: _setRankingTextColor(index))),
+                                            const Text('     |     ', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                                            Text('${snapshot.data?[index]?.userExp}  pts',
+                                              style: const TextStyle(fontSize: 16, color: Colors.black),
+                                            ),
+                                          ],
+                                        ),
+                                        Image(
+                                            width: MediaQuery.of(context).size.width * 0.75,
+                                            image: AssetImage('assets/images/line_divider.png')
+                                        ),
+                                      ],
                                     ),
-                                    Text('${index + 1}위',
-                                        style: TextStyle(fontSize: 16, color: _setRankingTextColor(index))),
-                                    const Text('     |     ', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
-                                    Text('${rankedUserList[index].userExp}  pts',
-                                      style: const TextStyle(fontSize: 16, color: Colors.black),
-                                    ),
-                                  ],
-                                ),
-                                Image(
-                                    width: MediaQuery.of(context).size.width * 0.75,
-                                    image: AssetImage('assets/images/line_divider.png')
-                                ),
-                              ],
-                            ),
-                          );
+                                  );
+                                }
+                              }
+                            );
                         } else {
-                          return Container(
-                            color: _setMyRankingFieldBackground(index),
-                            child: Column(
-                              children: [
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    Container(
-                                      width: 150,
-                                      height: 40,
-                                      margin: EdgeInsets.only(left: 35),
-                                      alignment: Alignment.centerLeft,
-                                      child: Text('  ${rankedUserList[index].userName}',
-                                        style: TextStyle(fontSize: 16, color: Colors.black,),),
-                                    ),
-                                    Text('${index + 1}위',
-                                        style: TextStyle(fontSize: 16, color: _setRankingTextColor(index))),
-                                    const Text('     |     ', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
-                                    Text('${rankedUserList[index].userExp}  pts',
-                                      style: const TextStyle(fontSize: 16, color: Colors.black),
-                                    ),
-                                  ],
-                                ),
-                                Image(
-                                  width: MediaQuery.of(context).size.width * 0.75,
-                                  image: AssetImage('assets/images/line_divider.png')
-                                ),
-                              ],
-                            ),
-                          );
+                          return const CircularProgressIndicator();
                         }
-                      }
+                      },
                     ),
                   ),
                   const SizedBox(
